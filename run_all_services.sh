08c584eda3f7576c1f33dd9697350b7029df4200
mvzhuravlev <mvzhuravlev@phoenixit.ru>
2019-12-24 23:24:59 +0300

    #18 commtit

--- run_all_services.sh
+++ run_all_services.sh
@@ -20,8 +20,9 @@ sudo java -jar ./bdwriters/bdinvalidwriterpayment/target/bd-invalid-writer-payme
 sudo java -jar ./counter/counterservices/target/counter-topic-services.jar &
 
 
-sudo ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class ru.otus.de.project.streamgeo.GeoStream ./target/scala-2.11/streamgeo-assembly-0.1.jar &
-
-sudo ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class ru.otus.de.project.streamsms.SmsStream ./target/scala-2.11/streamsms-assembly-0.1.jar &
-
-sudo ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class ru.otus.de.project.streampayment.PaymentStream ./target/scala-2.11/streampayment-assembly-0.1.jar &
+cd ./streams/streamgeo && sudo ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class ru.otus.de.project.streamgeo.GeoStream ./target/scala-2.11/streamgeo-assembly-0.1.jar &
+cd ../..
+cd ./streams/streamsms && sudo ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class ru.otus.de.project.streamsms.SmsStream ./target/scala-2.11/streamsms-assembly-0.1.jar &
+cd ../..
+cd ./streams/streampayment && sudo ~/spark-2.4.4-bin-hadoop2.7/bin/spark-submit --class ru.otus.de.project.streampayment.PaymentStream ./target/scala-2.11/streampayment-assembly-0.1.jar &
+cd ../..
